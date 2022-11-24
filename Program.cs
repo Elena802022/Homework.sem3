d0e350b7012f849cd1d25c530edf5d1f09bc7030
Elena <El.klod@bk.ru>
2022-11-24 14:57:23 +0100

    new files

---
+++ Program.cs
@@ -0,0 +1,37 @@
+﻿//Homework. Seminar 3
+// Task 19
+/*
+Console.WriteLine("Enter a number");
+string num = (Console.ReadLine()!);
+if (num.Length ==5)
+{
+    if (num[0] == num[4] && num[1] == num[3])
+    {
+        Console.WriteLine("Yes");
+    }
+    else
+    {
+        Console.WriteLine("No");
+
+    }
+}
+else
+{
+    Console.WriteLine("Entered wrong number");
+}
+*/
+//Task 21
+/*
+int [] mas = {0, 1, 2, 3, 4, 5};
+double masR = Math.Sqrt(Math.Pow(mas[0] - mas[3],2) + Math.Pow(mas[1] - mas[4],2) + Math.Pow(mas[2] - mas[5],2));
+Console.WriteLine($"{masR:f2} ");
+*/
+//Task 23
+/*
+Console.WriteLine("Enter a number");
+int number = int.Parse(Console.ReadLine()!);
+for (int i=1; i<=number; i++)
+{
+    Console.WriteLine($"{Math.Pow(i,3)}, ");
+}
+*/
